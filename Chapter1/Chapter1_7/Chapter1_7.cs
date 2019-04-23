70b4e29a95c1874669d852cf7a222da1da22f2ce
Eric Zocher <ezocher@users.noreply.github.com>
2019-04-23 16:55:34 -0700

    First working sample of section 1.7

---
+++ Chapter1/Chapter1_7/Chapter1_7.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * https://github.com/ezocher/HigherOrderCsharp
+ *
+ * C# implementation of the code from Higher Order Perl by Mark Jason Dominus
+ * https://hop.perl.plover.com/
+ *
+ */
+
+
+
+using System;
+using AngleSharp.Html.Dom; // NuGet package
+using AngleSharp.Html.Parser;
+
+public class Chapter1_7
+{
+    // first parse - Higher Order Perl pp. 26-28
+    public static IHtmlDocument FirstParse()
+    {
+        var parser = new HtmlParser();
+        IHtmlDocument tree = parser.ParseDocument(HTMLSamples.Simple_Page26);
+        return tree;
+    }
+
+    public static void Demo_First_Parse()
+    {
+        Console.WriteLine("\n--------------- Chapter 1.7 First parse ---------------");
+
+        var tree = FirstParse();
+        Console.WriteLine(tree.DocumentElement.OuterHtml);
+        Console.WriteLine("--------");
+        foreach (var element in tree.All)
+        {
+            Console.WriteLine($"{element.GetType()} = '{element.TextContent}'\n");
+        }
+        Console.WriteLine("--------");
+        var elements = tree.QuerySelectorAll("font");
+        Console.WriteLine($"Number of font tags = {elements.Length}");
+
+    }
+
+
+}
+
+public class HTMLSamples
+{
+    public const string EmptyHeader = @"
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <title></title> 
+</head> 
+<body>
+";
+
+    public const string EmptyFooter = @"
+</body> 
+</html>
+";
+
+    public const string Simple_Page26 = @"
+<h1>What Junior Said Next</h1>
+
+<p>But I don't <font size=3 color=""red"">want</font>
+to go to bed now</p>";
+
+}
--- Chapter1/Chapter1_7/Program.cs
+++ Chapter1/Chapter1_7/Program.cs
@@ -6,36 +6,14 @@
  *
  */
 
+// Uses NuGet package AngleSharp
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string[] pathList = {
-            @"c:\nosuchfileexists",
-            @"C:\Temp\test.txt",
-            Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic),
-            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)};
-
-        //TotalSize.Demo(pathList);   // This demo runs multiple paths since it doesn't do much for each one
-
-        /*
-         * The Demo()s below all take one path - some of them are long running or produce a lot of output depending on your numbers of files
-         * 
-         */
-
-        string path = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
-        // Other directories you might want to try
-        //      Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        //      Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic);
-        //      Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        //      Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        //      @"C:\Users\"
-
-        //PrintAll.Demo(path);
-        Console.WriteLine("Press any key to exit");
-        Console.ReadKey(true);
+        Chapter1_7.Demo_First_Parse();
     }
 }
